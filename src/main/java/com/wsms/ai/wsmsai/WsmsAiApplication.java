c46fb621ebfe43eab68850346134425eae82a0d0
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-05-23 16:09:36 +0100

    setup commit

---
+++ src/main/java/com/wsms/ai/wsmsai/WsmsAiApplication.java
@@ -0,0 +1,13 @@
+package com.wsms.ai.wsmsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmsAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsmsAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsms/ai/wsmsai/WsmsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsms.ai.wsmsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmsAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
